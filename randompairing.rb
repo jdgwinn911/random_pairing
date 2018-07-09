b3ec65e9ca9131066731b08249f6194c2ef1063e
Josh Gwinn <jdgwinn911@gmail.com>
2018-07-09 15:51:25 -0400

    didn't put refute instead of assert

--- randompairing.rb
+++ randompairing.rb
@@ -1,5 +1,8 @@
 def names()
-      people_names = ["billy", "jimbob", "spike", "wilburt", "carol"]
-    people_names.shuffle
-    p people_names
+      people_names = ["billy", "jimbob", "fredward", "wilburt", "carol", "jimmothy"]
+      people_names.sample(6)
+    #   people_names2 = []
+    
+    #   people_names.shuffle
+    # people_names.
 end
--- test_randompairing.rb
+++ test_randompairing.rb
@@ -5,4 +5,12 @@ class RandomPairing < Minitest::Test
     def test_that_array_is_an_array
         assert_equal(Array, names().class)
     end
+
+    def test_that_array_has_been_shuffled
+        assert_equal(["billy", "jimbob", "fredward", "wilburt", "carol", "jimmothy"], names())
+    end
+
+
+
+
 end
