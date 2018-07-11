c7cacbde6b2bce268591f4765ec10a73f41434b9
Josh Gwinn <jdgwinn911@gmail.com>
2018-07-11 10:31:25 -0400

    expected array but got nil class

--- randompairing.rb
+++ randompairing.rb
@@ -1,15 +1,14 @@
 def pairing_names(names)
     start = names.shuffle
-#     if start.length.even? == true
-#         newarr = start.each_slice(2).to_a
-#     else
-#         newarr = start.each_slice(2).to_a
-#         newarr2 = newarr.last(2)
-#         newarr2 = newarr2[0] + newarr2[1]
-#         newarr = newarr[0..-3]
-#         newarr.push(newarr2)
-#     end
-# end
-# p pairing_names(["billy", "fredward", "breadward", "peter", "zatch"])
-
+    if start.length.even? == true
+        newarr = start.each_slice(2).to_a
+    # else
+    #     newarr = start.each_slice(2).to_a
+    #     newarr2 = newarr.last(2)
+    #     newarr2 = newarr2[0] + newarr2[1]
+    #     newarr = newarr[0..-3]
+    #     newarr.push(newarr2)
+    end
 end
+p pairing_names(["billy", "fredward", "breadward", "peter", "zatch"])
+
--- test_randompairing.rb
+++ test_randompairing.rb
@@ -11,6 +11,10 @@ class RandomPairing < Minitest::Test
         refute_equal(["billy", "fredward", "breadward", "peter", "zatch"], pairing_names(["billy", "fredward", "breadward", "peter", "zatch"]))
     end
 
+    def test_that_array_has_been_sliced
+        refute_equal([["billy", "fredward"], ["breadward", "peter", "zatch"]], pairing_names(["billy", "fredward", "breadward", "peter", "zatch"]))
+    end
+
 
 
 
