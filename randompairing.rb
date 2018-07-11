1ecf3a573be4d224d9217b113579f8fb50f58fb5
Josh Gwinn <jdgwinn911@gmail.com>
2018-07-11 10:33:06 -0400

    tinkering around,made it pass

--- randompairing.rb
+++ randompairing.rb
@@ -2,9 +2,9 @@ def pairing_names(names)
     start = names.shuffle
     if start.length.even? == true
         newarr = start.each_slice(2).to_a
-    # else
-    #     newarr = start.each_slice(2).to_a
-    #     newarr2 = newarr.last(2)
+    else
+        newarr = start.each_slice(2).to_a
+        newarr2 = newarr.last(2)
     #     newarr2 = newarr2[0] + newarr2[1]
     #     newarr = newarr[0..-3]
     #     newarr.push(newarr2)
